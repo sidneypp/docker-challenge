0935039f2e744dcacbdc6d2eb91b7ba40049a799
Sidney Oliveira <oliveira.sidney96@gmail.com>
2021-10-11 03:57:13 -0300

    Install and config prisma

--- nginx-com-node/src/server.ts
+++ nginx-com-node/src/server.ts
@@ -1,9 +1,25 @@
-import express from "express";
+import express from 'express';
+import { PrismaClient } from '@prisma/client'
+import { uniqueNamesGenerator, Config, names } from 'unique-names-generator'
 
 const server = express();
+const prisma = new PrismaClient()
+const config: Config = {
+  dictionaries: [names]
+}
 
-server.get("/", (request, response) => {
-  return response.send("<h1>Full Cycle Rocks</h1>");
+server.get('/', async (request, response) => {
+  let htmlResponse = '<h1>Full Cycle Rocks!</h1>'
+  await prisma.people.create({
+    data: {
+      name: uniqueNamesGenerator(config)
+    }
+  })
+  const peoples = await prisma.people.findMany()
+  peoples.forEach(people => {
+    htmlResponse += `<li>${people.name}</li>`
+  })
+  return response.send(htmlResponse);
 });
 
 export default server;
